6f5ef6455d097b94823e3e5cd49b87b078aea5c1
Debjyoti Guha <debajyotiguha11@gmail.com>
2017-08-10 18:56:37 +0530

    Time Optimization Algoritm for better performence

--- ReadDisplay.c
+++ ReadDisplay.c
@@ -1,43 +1,40 @@
 /*
 Author: Debjyoti Guha
 Date: 09/08/2017
+Update: 10/08/2017
+Update includes Size and Time optimization algorithm.
 Description: This will read and display n int no using an arrey.
 */
 
 #include<stdio.h>
-int read(int b[], int l);	//function declaration
+void read(int b[], int l);	//function declaration
 void disp(int c[], int m);	//function declaration
 void main()	// Main function
 {
-	int i, n, a[50], x[50];
-	printf("Enter no of elm\n");
+	int i, n, a[50];
+	printf("Enter number of elm\n");
 	scanf("%d", &n);
-	printf("Enter %d elements\n", n);
-	for(i=0;i<n;i++)
-	{
-	x[i]=read(a,n);	// call read function
-	}
-	disp(x,n);	// call disp function
-	
+	read(a,n);	// call read function
+	disp(a,n);	// call disp function
+
 }
 
-int read(int b[], int l) //called function
+void read(int b[], int l) //called function
 {
-
 	int i;
-	for(i=0;i<l;i++)
-	{
-		scanf("%d", &b[i]);
-		return b[i];
-	}
+	printf("Enter %d Intiger elements\n", l);
+        	for(i=0;i<l;i++)
+        	{
+			scanf("%d", &b[i]);
+		}
 }
 
 void disp(int c[], int m) //called function
 {
 	int i;
-	printf("Entered elm are\n");
-	for(i=0;i<m;i++)
-	{
-		printf("%d\n", c[i]);
-	}
+	printf("Entered elements are\n");
+		for(i=0;i<m;i++)
+		{
+			printf("%d\n", c[i]);
+		}
 }
